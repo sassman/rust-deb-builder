bb91ca20529ddfbdf2926aba3e3e7425f0715d86
Sven Assmann <sven.assmann.it@gmail.com>
2021-12-06 10:05:19 +0100

    fix(action:input): fix input variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -ex
 
-if [ -z "${PACKAGE}" ]; then
+if [ -z "${INPUT_PACKAGE}" ]; then
     PACKAGE=""
 else
-    PACKAGE="-p ${PACKAGE}"
+    PACKAGE="-p ${INPUT_PACKAGE}"
 fi
 
-if [ -z "${TARGET}" ]; then
-    TARGET="x86_64-unknown-linux-musl"
+if [ -z "${INPUT_TARGET}" ]; then
+    INPUT_TARGET="x86_64-unknown-linux-musl"
 fi
 
-TARGET="--target ${TARGET}"
+TARGET="--target ${INPUT_TARGET}"
 [[ -n "${GITHUB_WORKSPACE}" ]] && cd ${GITHUB_WORKSPACE}
 
 rustc --version
